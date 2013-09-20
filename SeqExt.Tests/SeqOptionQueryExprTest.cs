5e2e682d3cdaa1c95ebc4875e6b9e25a65092839
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-09-20 18:59:15 +0900

    tmp

---
+++ SeqExt.Tests/SeqOptionQueryExprTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LangExt;
+using NUnit.Framework;
+using SeqExt.QueryExpr.SeqOption;
+
+namespace SeqExt.Tests
+{
+    [TestFixture]
+    public class SeqOptionQueryExprTest
+    {
+        [Test]
+        public void クエリ式が使える()
+        {
+            var xs = Range.FromTo(1, 5).ToSeq().Map(Option.Some);
+            var res =
+                from x in xs
+                from y in Option.Some("hoge")
+                select y + x;
+            Assert.That(res.GetOr(null).ToArray(), Is.EqualTo(new[] { "hoge1", "hoge2", "hoge3", "hoge4", "hoge5" }));
+        }
+    }
+}
--- SeqExt/QueryExpr/SeqOption/SeqOptionQueryExpr.cs
+++ SeqExt/QueryExpr/SeqOption/SeqOptionQueryExpr.cs
@@ -25,5 +25,24 @@ namespace SeqExt.QueryExpr.SeqOption
             }
             return Option.Some(res.ToSeq());
         }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static Option<Seq<V>> SelectMany<T, U, V>(
+            this Option<Seq<T>> self, Func<T, Option<U>> f, Func<T, U, V> g)
+        {
+            if (self.IsNone)
+                return Option.None;
+            var xs = self.GetValue(); // 上でチェック済みのため、安全
+            var res = new System.Collections.Generic.List<V>();
+            foreach (var x in xs)
+            {
+                var v = f(x).Bind(u => Option.Some(g(x, u)));
+                if (v.IsNone)
+                    return Option.None;
+                res.Add(v.GetValue()); // 上でチェック済みのため、安全
+            }
+            return Option.Some(res.ToSeq());
+        }
+
     }
 }
