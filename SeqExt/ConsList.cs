5fcc6ae4cba8a72456f2f4a013d3ffa108128644
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-07 09:41:26 +0900

    ドキュメンテーションコメントを追加 refs #1

--- SeqExt/ConsList.cs
+++ SeqExt/ConsList.cs
@@ -1,23 +1,41 @@
 ﻿using System;
+using System.ComponentModel;
 using LangExt;
 
 namespace SeqExt
 {
+    /// <summary>
+    /// ConsListに対する関数を提供します。
+    /// </summary>
     public static class ConsList
     {
+        /// <summary>
+        /// 任意のConsListに変換可能な空のConsListです。
+        /// </summary>
         public static readonly ConsList<Placeholder> Nil = ConsList<Placeholder>.Nil;
 
+        /// <summary>
+        /// 先頭要素と末尾リストからConsListを構築します。
+        /// </summary>
         public static ConsList<T> Cons<T>(this T self, ConsList<T> rest)
         {
             return new ConsList<T>.Cons(self, rest);
         }
 
+        /// <summary>空のConsListを作ります。</summary>
         public static ConsList<T> Create<T>() { return ConsList<T>.Nil; }
+        /// <summary>要素が1つ含まれたConsListを作ります。</summary>
         public static ConsList<T> Create<T>(T x1) { return Cons(x1, ConsList<T>.Nil); }
+        /// <summary>要素が2つ含まれたConsListを作ります。</summary>
         public static ConsList<T> Create<T>(T x1, T x2) { return Cons(x1, Cons(x2, ConsList<T>.Nil)); }
+        /// <summary>要素が3つ含まれたConsListを作ります。</summary>
         public static ConsList<T> Create<T>(T x1, T x2, T x3) { return Cons(x1, Cons(x2, Cons(x3, ConsList<T>.Nil))); }
+        /// <summary>要素が4つ含まれたConsListを作ります。</summary>
         public static ConsList<T> Create<T>(T x1, T x2, T x3, T x4) { return Cons(x1, Cons(x2, Cons(x3, Cons(x4, ConsList<T>.Nil)))); }
 
+        /// <summary>
+        /// 引数に指定された要素が含まれたConsListを作ります。
+        /// </summary>
         public static ConsList<T> Create<T>(params T[] values)
         {
             var res = ConsList<T>.Nil;
@@ -29,13 +47,32 @@ namespace SeqExt
             }
             return res;
         }
+
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static new bool Equals(object a, object b) { return object.Equals(a, b); }
+
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static new bool ReferenceEquals(object a, object b) { return object.ReferenceEquals(a, b); }
     }
 
+    /// <summary>
+    /// 任意の要素を格納可能な単方向の連結リストです。
+    /// このシーケンスはイミュータブルです。
+    /// </summary>
     public abstract class ConsList<T> : Seq<T>, IEquatable<ConsList<T>>
     {
         ConsList() { }
+        /// <summary>
+        /// 空のConsListです。
+        /// </summary>
         public static readonly ConsList<T> Nil = new NilType();
 
+        /// <summary>
+        /// Placeholder型の空のConsListを、任意の型のConsListに暗黙変換します。
+        /// 空ではないリストを渡した場合は、例外が発生します。
+        /// </summary>
         public static implicit operator ConsList<T>(ConsList<Placeholder> nil)
         {
             if (nil.IsNotEmpty())
@@ -43,17 +80,47 @@ namespace SeqExt
             return ConsList<T>.Nil;
         }
 
+        /// <summary>
+        /// 現在のオブジェクトにConsListを連結したConsListを生成します。
+        /// </summary>
         public abstract ConsList<T> Append(ConsList<T> other);
 
+        /// <summary>
+        /// 現在のオブジェクトが空かどうかを返します。 
+        /// </summary>
         public abstract bool IsEmpty { get; }
+        /// <summary>
+        /// 現在のオブジェクトを分解して、
+        /// 要素を持つ場合はその要素と残りのリストを使った処理を、
+        /// 要素がない場合はその場合の処理を実行します。
+        /// 1つしか要素がなかった場合、残りのリストにはNilが渡されます。
+        /// </summary>
+        /// <param name="Cons">リストに要素が含まれる場合の処理</param>
+        /// <param name="Nil">リストに要素が含まれない場合の処理</param>
+        /// <returns>処理の結果</returns>
         public abstract U Match<U>(Func<T, ConsList<T>, U> Cons, Func<U> Nil);
 
+        /// <summary>
+        /// 現在のオブジェクトが空でないかどうかを返します。
+        /// </summary>
         public bool IsNotEmpty { get { return this.IsEmpty == false; } }
+        /// <summary>
+        /// 現在のオブジェクトを分解して、
+        /// 要素を持つ場合はその要素と残りのリストを使った処理を、
+        /// 要素がない場合はその場合の処理を実行します。
+        /// 1つしか要素がなかった場合、残りのリストにはNilが渡されます。
+        /// </summary>
+        /// <param name="Cons">リストに要素が含まれる場合の処理</param>
+        /// <param name="Nil">リストに要素が含まれない場合の処理</param>
         public void Match(Action<T, ConsList<T>> Cons, Action Nil)
         {
             Match((x, xs) => { Cons(x, xs); return Unit._; }, () => { Nil(); return Unit._; });
         }
 
+        /// <summary>
+        /// 現在のオブジェクトを走査するためのEnumeratorを取得します。
+        /// </summary>
+        /// <returns></returns>
         public System.Collections.Generic.IEnumerator<T> GetEnumerator()
         {
             return new Enumerator(this);
@@ -144,7 +211,12 @@ namespace SeqExt
             public void Dispose() { }
         }
 
-
+        /// <summary>
+        /// 現在のオブジェクトが、同じ型の別のオブジェクトと等しいかどうかを判定します。 
+        /// ConsList.Nilで返された値と、型付きのNilを比較した場合にfalseが返される点に注意してください。
+        /// </summary>
+        /// <param name="other">このオブジェクトと比較するConsList</param>
+        /// <returns>現在のオブジェクトがobjで</returns>
         public bool Equals(ConsList<T> other)
         {
             if (this.IsEmpty)
@@ -168,6 +240,8 @@ namespace SeqExt
             return true;
         }
 
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj)
         {
             var other = obj as ConsList<T>;
@@ -176,6 +250,8 @@ namespace SeqExt
             return this.Equals(other);
         }
 
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode()
         {
             var hash = 31;
@@ -184,6 +260,9 @@ namespace SeqExt
             return hash;
         }
 
+        /// <summary>
+        /// このオブジェクトの文字列表現を取得します。
+        /// </summary>
         public override string ToString()
         {
             if (this.IsEmpty)
@@ -194,5 +273,13 @@ namespace SeqExt
             buf.Remove(buf.Length - 2, 2);
             return buf.Append(")").ToString();
         }
+
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static new bool Equals(object a, object b) { return object.Equals(a, b); }
+
+        /// <summary>使用しません。</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static new bool ReferenceEquals(object a, object b) { return object.ReferenceEquals(a, b); }
     }
 }
